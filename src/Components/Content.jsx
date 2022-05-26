cc3528524b46cc4776cafd6906a6294025e1b513
Burstniart <v4gr33n@hotmail.com>
2022-05-26 14:28:47 -0500

    Pokemon by type now appear, but only the first time

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -5,37 +5,28 @@ import SearchParams from "./SearchParams";
 
 const Content = () => {
 
-  const [pokemons, setPokemons] = useState([])
+  // const [pokemons, setPokemons] = useState([])
 
-  useEffect(() => {
-    requestPokemon()
-  }, [])
+  // useEffect(() => {
+  //   requestPokemon()
+  // }, [])
 
-  async function requestPokemon() {
-    const res = await fetch(
-      `https://pokeapi.co/api/v2/pokemon/282/`
-    )
-    const json = await res.json()
+  // async function requestPokemon() {
+  //   const res = await fetch(
+  //     `https://pokeapi.co/api/v2/type/fairy/`
+  //   )
+  //   const json = await res.json()
 
-    console.log(json)
+  //   // console.log(json)
 
-    console.log(json.name)
-    console.log(json.sprites.other["official\-artwork"].front_default)
-    // console.log(json.types)
-    const letype = json.types.map((typelol) => {
-      return typelol.type.name.toString()
-    });
+   
 
-    // console.log(letype.toString())
-    const lmao = {
-      name: json.name,
-      types: letype,
-      image: json.sprites.other["official\-artwork"].front_default
-    }
-
-    setPokemons(lmao)
+  //   setPokemons(json)
     
-  }
+  // }
+
+  console.log("Hiiiiiiiii")
+  console.log(SearchParams.lePokemons)
   
   return (
 
@@ -46,19 +37,9 @@ const Content = () => {
         ~~~ Pokemon goes here ~~~
       </h2>
       <div className="columns">
-      {/* <Pokemon name="Gardevoir" 
-      type="Psychic" 
-      image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/282.png" 
-      /> */}
-
-      
-
-      {/* <Pokemon name={pokemons.name}
-      type={pokemons.types}
-      image={pokemons.image}
-      /> */}
 
       <Pokemon url={"https://pokeapi.co/api/v2/pokemon/282/"}/>
+
       </div>
     </section>
 
--- src/Components/SearchParams.jsx
+++ src/Components/SearchParams.jsx
@@ -5,65 +5,100 @@ const TYPES =["normal", "fighting", "flying", "poison","ground", "rock", "bug", 
 "grass", "electric", "psychic", "ice", "dragon", "dark", "fairy", "unknown", "shadow"] 
 
 const SearchParams = () => {
-  // const type = "psychic"
-  // const pokemon= "garde"
   const [pokemon, setPokemon] = useState("")
   const [type, setType] = useState("")
-  const [type2, setType2] = useState("")
   const types = []
-  // const [pokemon, setPokemon] = useState([])
+  const [lePokemons, setLePokemons] = useState([])
+
+  
+
+  useEffect(() => {
+    requestLePokemons()
+  }, [])
+
+  async function requestLePokemons() {
+    const res = await fetch(
+      `https://pokeapi.co/api/v2/type/${type}/`
+    )
+    const json = await res.json()
+
+    console.log(json.pokemon)
+
+    
+    setLePokemons(json.pokemon)
+    
+  }
+  
   return (
     <div className="search-params">
-      <div className="field">
-        <label className="label" htmlFor="pokemon">
-          Pokémon {pokemon}
-          </label>
-        <div className="control">
-          <input className="input" 
-          id="pokemon"
-          value={pokemon}
-          type="text"
-          placeholder="Pokémon"
-          onChange={(e) => setPokemon(e.target.value)}
-          />
-
-        </div>
-      </div>
-      <div className="field">
-        <label htmlFor="type" className="label">Type
+      <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        requestLePokemons();
+      }}
+      >
+        <div className="field">
+          <label className="label" htmlFor="pokemon">
+            Pokémon {pokemon}
+            </label>
           <div className="control">
-            <div className="select">
-              <select
-              id="type"
-              value={type}
-              onChange={(e) => {
-                setType(e.target.value)
-                setType2("")
+            <input className="input" 
+            id="pokemon"
+            value={pokemon}
+            type="text"
+            placeholder="Pokémon"
+            onChange={(e) => setPokemon(e.target.value)}
+            />
 
-              }}
-              onBlur={(e) => {
-                setType(e.target.value)
-                setType2("")
-              }}
-              >
-                <option/>
-                
-                {TYPES.map((type) => (
-                  <option key={type} value={type}>
-                    {type}
-                  </option> 
-                ))
-                }
-              </select>
-            </div>
           </div>
-        </label>
-      </div>
-      <div className="field is-grouped">
-        <div className="control">
-          <button className="button is-link">Search</button>
         </div>
-      </div>
+        <div className="field">
+          <label htmlFor="type" className="label">Type
+            <div className="control">
+              <div className="select">
+                <select
+                  id="type"
+                  value={type}
+                  onChange={(e) => {
+                    setType(e.target.value)
+
+                    }}
+                  onBlur={(e) => {
+                    setType(e.target.value)
+                    }}
+                >
+                    <option/>
+                    
+                    {TYPES.map((type) => (
+                      <option key={type} value={type}>
+                        {type}
+                      </option> 
+                    ))
+                    }
+                </select>
+              </div>
+            </div>
+          </label>
+        </div>
+        <div className="field is-grouped">
+          <div className="control">
+        
+            <button className="button is-link">Search</button>
+          </div>
+        </div>
+      </form>
+        
+        {
+        
+        lePokemons.map(lePokemon => (
+          <Pokemon url={lePokemon.pokemon.url} />
+        ))
+        
+        }
+  
+      
+      
+      
     </div>
   )
 }
